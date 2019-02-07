0b3ae9f6f7712492ecdb0fdfc418f5119755cab2
Juber Ahmed <a.juber@hotmail.com>
2019-02-07 18:40:45 -0500

    work in progress

---
+++ src/app/ajuber/Main.java
@@ -0,0 +1,23 @@
+package app.ajuber;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        Stack data = new Stack();
+        data.pop();
+        data.push("3");
+        data.print();
+        data.pop();
+
+//        data.push("7");
+//        System.out.println("Size is " + data.size());
+//        System.out.println("The element popped " +data.pop());
+//        System.out.println("The element popped " +data.pop());
+//
+//        System.out.println("The element popped " +data.pop());
+//
+//        data.print();
+    }
+}
